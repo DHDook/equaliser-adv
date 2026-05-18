8452dac257e4170b874761eb343267ed2e89730f
DHDook <dhasandras@icloud.com>
2026-05-18 18:09:48 -0400

    Add missing UpdateConstants: GITHUB_REPO_URL and DOCS_URL for About tab links

--- src/updates/UpdateConstants.swift
+++ src/updates/UpdateConstants.swift
@@ -11,5 +11,11 @@ let UPDATE_CHECK_API_URL = "https://api.github.com/repos/cvknage/equaliser/relea
 /// Download page for users.
 let UPDATE_DOWNLOAD_URL = "https://equaliser.knage.net"
 
+/// GitHub repository URL.
+let GITHUB_REPO_URL = "https://github.com/cvknage/equaliser"
+
+/// Documentation page URL.
+let DOCS_URL = "https://equaliser.knage.net/docs.html"
+
 /// HTTP timeout interval for the update check request (seconds).
 let UPDATE_CHECK_TIMEOUT_INTERVAL: TimeInterval = 15
